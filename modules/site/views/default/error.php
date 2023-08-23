4a5bbdaff86005f8998b30b8fa0c3fc708ec9e1f
Pavel Klochkov <pv.klochkov@gmail.com>
2023-08-23 22:58:17 +0300

    create page error 404

---
+++ modules/site/views/default/error.php
@@ -0,0 +1,13 @@
+<?php
+$this->title = '404 CAFFÉ L\'Antico';
+?>
+<article class="d-flex justify-content-center align-items-center page pt-5">
+    <section class="text-light text-center shadow px-5">
+        <div class="fs-1 caffelantico">Ошибка</div>
+        <div class="display-1 caffelantico">404</div>
+        <p class="fs-4 caffelantico">Страница не найдена</p>
+        <img src="/img/site/IMG-20170302-WA0006.jpg" alt="image" class="shadow shadow-lg img-fluid">
+        <p class="fs-4 mt-3">Похоже администратор сайта увлёкся CAFFÉ L'Antico... <br>
+            как только закончит эта страница найдётся.</p>
+    </section>
+</article>
