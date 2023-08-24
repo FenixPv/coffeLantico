7688eb0b261201908a935541f96739b098d048ed
Pavel Klochkov <pv.klochkov@yandex.ru>
2023-08-24 16:06:14 +0300

    create migrate user

---
+++ modules/user/models/User.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace app\modules\user\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "user".
+ *
+ * @property int $id
+ * @property int $created_at
+ * @property int $updated_at
+ * @property string $username
+ * @property string|null $auth_key
+ * @property string|null $email_confirm_token
+ * @property string $password_hash
+ * @property string|null $password_reset_token
+ * @property string $email
+ * @property int $status
+ */
+class User extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'user';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['created_at', 'updated_at', 'username', 'password_hash', 'email'], 'required'],
+            [['created_at', 'updated_at', 'status'], 'integer'],
+            [['username', 'email_confirm_token', 'password_hash', 'password_reset_token', 'email'], 'string', 'max' => 255],
+            [['auth_key'], 'string', 'max' => 32],
+            [['username'], 'unique'],
+            [['email'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+            'username' => 'Username',
+            'auth_key' => 'Auth Key',
+            'email_confirm_token' => 'Email Confirm Token',
+            'password_hash' => 'Password Hash',
+            'password_reset_token' => 'Password Reset Token',
+            'email' => 'Email',
+            'status' => 'Status',
+        ];
+    }
+}
