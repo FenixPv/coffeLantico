a76c0893f7a69792c1787fe25cb5d73c4ab169ca
Pavel Klochkov <pv.klochkov@yandex.ru>
2023-08-25 22:13:42 +0300

    Перенесён и изменён класс User

--- modules/user/models/User.php
+++ modules/user/models/User.php
@@ -3,6 +3,11 @@
 namespace app\modules\user\models;
 
 use Yii;
+use yii\base\NotSupportedException;
+use yii\behaviors\TimestampBehavior;
+use yii\db\ActiveRecord;
+use yii\helpers\ArrayHelper;
+use yii\web\IdentityInterface;
 
 /**
  * This is the model class for table "user".
@@ -16,49 +21,176 @@ use Yii;
  * @property string $password_hash
  * @property string|null $password_reset_token
  * @property string $email
+ * @property-read mixed $statusName
+ * @property-write mixed $password
+ * @property-read null|string $authKey
  * @property int $status
  */
-class User extends \yii\db\ActiveRecord
+class User extends ActiveRecord implements IdentityInterface
 {
-    /**
-     * {@inheritdoc}
-     */
-    public static function tableName()
+    const STATUS_WAIT    = 2;
+    const STATUS_ACTIVE  = 1;
+    const STATUS_BLOCKED = 0;
+
+    public function behaviors(): array
+    {
+        return [
+            TimestampBehavior::class,
+        ];
+    }
+
+    public static function tableName(): string
     {
         return 'user';
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function rules()
+    public function rules(): array
     {
         return [
-            [['created_at', 'updated_at', 'username', 'password_hash', 'email'], 'required'],
-            [['created_at', 'updated_at', 'status'], 'integer'],
-            [['username', 'email_confirm_token', 'password_hash', 'password_reset_token', 'email'], 'string', 'max' => 255],
-            [['auth_key'], 'string', 'max' => 32],
-            [['username'], 'unique'],
-            [['email'], 'unique'],
+//            [['created_at', 'updated_at', 'username', 'password_hash', 'email'], 'required'],
+//            [['created_at', 'updated_at', 'status'], 'integer'],
+//            [['username', 'email_confirm_token', 'password_hash', 'password_reset_token', 'email'], 'string', 'max' => 255],
+//            [['auth_key'], 'string', 'max' => 32],
+//            [['username'], 'unique'],
+//            [['email'], 'unique'],
+
+            ['username', 'required'],
+            ['username', 'match', 'pattern' => '#^[\w_-]+$#is'],
+            ['username', 'unique', 'targetClass' => self::class, 'message' => 'This username has already been taken.'],
+            ['username', 'string', 'min' => 2, 'max' => 255],
+
+            ['email', 'required'],
+            ['email', 'email'],
+            ['email', 'unique', 'targetClass' => self::class, 'message' => 'This email address has already been taken.'],
+            ['email', 'string', 'max' => 255],
+
+            ['status', 'integer'],
+            ['status', 'default', 'value' => self::STATUS_ACTIVE],
+            ['status', 'in', 'range' => array_keys(self::getStatusesArray())],
         ];
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function attributeLabels()
+    public function attributeLabels(): array
     {
         return [
-            'id' => 'ID',
-            'created_at' => 'Created At',
-            'updated_at' => 'Updated At',
-            'username' => 'Username',
-            'auth_key' => 'Auth Key',
-            'email_confirm_token' => 'Email Confirm Token',
-            'password_hash' => 'Password Hash',
-            'password_reset_token' => 'Password Reset Token',
-            'email' => 'Email',
-            'status' => 'Status',
+            'id'         => 'ID',
+            'created_at' => 'Создан',
+            'updated_at' => 'Обновлён',
+            'username'   => 'Имя пользователя',
+            'email'      => 'Email',
+            'status'     => 'Статус',
         ];
+    }
+
+    public static function getStatusesArray(): array
+    {
+        return [
+            self::STATUS_WAIT    => 'Ожидает подтверждения',
+            self::STATUS_ACTIVE  => 'Активен',
+            self::STATUS_BLOCKED => 'Заблокирован',
+        ];
+    }
+
+    public function getStatusName(): mixed
+    {
+        return ArrayHelper::getValue(self::getStatusesArray(), $this->status);
+    }
+
+    /**
+     * @param $id
+     * @return IdentityInterface|null
+     */
+    public static function findIdentity($id): ?IdentityInterface
+    {
+        return static::findOne(condition: ['id' => $id, 'status' => self::STATUS_ACTIVE]);
+    }
+
+    /**
+     * @param $token
+     * @param $type
+     * @return IdentityInterface|null
+     * @throws NotSupportedException
+     */
+    public static function findIdentityByAccessToken($token, $type = null): ?IdentityInterface
+    {
+        throw new NotSupportedException('findIdentityByAccessToken is not implemented.');
+    }
+
+    public function getId(): int
+    {
+        return $this->getPrimaryKey();
+    }
+
+    public function getAuthKey(): ?string
+    {
+        return $this->auth_key;
+    }
+
+    public function validateAuthKey($authKey): ?bool
+    {
+        return $this->getAuthKey() === $authKey;
+    }
+
+    public static function findByUsername($username): ?User
+    {
+        return static::findOne(['username' => $username]);
+    }
+
+    public function validatePassword($password): bool
+    {
+        return Yii::$app->security->validatePassword($password, $this->password_hash);
+    }
+
+    public function setPassword($password): void
+    {
+        $this->password_hash = Yii::$app->security->generatePasswordHash($password);
+    }
+
+    public function generateAuthKey(): void
+    {
+        $this->auth_key = Yii::$app->security->generateRandomString();
+    }
+
+    public function beforeSave($insert): bool
+    {
+        if (parent::beforeSave($insert)) {
+            if ($insert) {
+                $this->generateAuthKey();
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public static function isPasswordResetTokenValid($token): bool
+    {
+        if (empty($token)) {
+            return false;
+        }
+        $expire = Yii::$app->params['user.passwordResetTokenExpire'];
+        $parts = explode('_', $token);
+        $timestamp = (int) end($parts);
+        return $timestamp + $expire >= time();
+    }
+
+    public static function findByPasswordResetToken($token): ?User
+    {
+        if (!static::isPasswordResetTokenValid($token)) {
+            return null;
+        }
+        return static::findOne([
+            'password_reset_token' => $token,
+            'status' => self::STATUS_ACTIVE,
+        ]);
+    }
+
+    public function generatePasswordResetToken(): void
+    {
+        $this->password_reset_token = Yii::$app->security->generateRandomString() . '_' . time();
+    }
+
+    public function removePasswordResetToken(): void
+    {
+        $this->password_reset_token = null;
     }
 }
